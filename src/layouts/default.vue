f7d2904b310a44a3f6b0ce89e6b79f97fa559cba
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2021-02-05 23:54:24 +0900

    :wrench: CompositionAPIの設定を追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,3 +3,9 @@
     <nuxt />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+
+export default defineComponent({})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <h1>yuarasino-homepage</h1>
+    <h1>{{ title }}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@nuxtjs/composition-api"
+
+export default defineComponent({
+  setup() {
+    const title = ref("yuarasino-homepage")
+
+    return { title }
+  },
+})
+</script>
