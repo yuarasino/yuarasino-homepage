101b0767cfa0421a135238072c8621af2880fd41
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-04-30 09:40:14 +0900

    :rocket: ヘッダーを追加

---
+++ src/constants.ts
@@ -0,0 +1,52 @@
+export const primaryLinks = [
+  {
+    title: "Top",
+    url: "/",
+  },
+  {
+    title: "Steps",
+    url: "/steps/",
+  },
+  {
+    title: "Works",
+    url: "/works/",
+  },
+  {
+    title: "Notes",
+    url: "/notes/",
+  },
+]
+
+export const secondaryLinks = [
+  {
+    title: "Top",
+    url: "/",
+  },
+  {
+    title: "Steps",
+    url: "/steps/",
+  },
+  {
+    title: "Works",
+    url: "/works/",
+  },
+  {
+    title: "Notes",
+    url: "/notes/",
+  },
+]
+
+export const iconLinks = [
+  {
+    icon: "i-fa6-brands-x-twitter",
+    url: "https://twitter.com/yuarasino",
+  },
+  {
+    icon: "i-fa6-brands-youtube",
+    url: "https://www.youtube.com/@yuarasino",
+  },
+  {
+    icon: "i-fa6-brands-github",
+    url: "https://github.com/yuarasino",
+  },
+]
