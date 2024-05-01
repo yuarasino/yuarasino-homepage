8338c490c6dbea059035abc904a2d8dd484d798c
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-05-02 08:25:14 +0900

    :rocket: TailwindCSSを導入

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 
+import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 
 /** @type {import("astro").AstroUserConfig} */
-export default defineConfig({})
+export default defineConfig({
+  integrations: [tailwind()],
+})
