17ff47c93913910c9606ada154191058dde5e62b
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-05-06 12:35:27 +0900

    :rocket: プロフィールセクションを作成

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from "astro:content"
+
+const profileCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      reading: z.string(),
+      spelling: z.string(),
+      position: z.string(),
+      avatar: image(),
+      diffs: image().array(),
+      birthday: z.string(),
+      funmark: z.string(),
+      funart: z.string(),
+      histories: z.string().array(),
+    }),
+})
+
+export const collections = {
+  profile: profileCollection,
+}
