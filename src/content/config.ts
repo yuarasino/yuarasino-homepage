9f9c638a3e01eba1a752091d85025124986e583c
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2024-05-08 06:25:59 +0900

    wip

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,6 +17,19 @@ const profileCollection = defineCollection({
     }),
 })
 
+const resumeCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      reading: z.string(),
+      spelling: z.string(),
+      position: z.string(),
+      avatar: image(),
+    }),
+})
+
 export const collections = {
   profile: profileCollection,
+  resume: resumeCollection,
 }
